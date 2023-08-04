2ba2bb07b7feb83b3377db1c8bc904fd1586bf89
Leandro Rocha <leandro.aerolito@gmail.com>
2023-08-04 09:49:06 -0300

    Calcular Média
    Duas opções para resolver o cálculo da média.

--- exercicios/exercicio-05.php
+++ exercicios/exercicio-05.php
@@ -6,10 +6,37 @@
     <title>Exercício 5 - Funções</title>
 </head>
 <body>
-     <h1>Exercício 05 - FUNÇÕES</h1>    
+    <h1>Exercício 05 - FUNÇÕES</h1>    
 
-     
+    <hr>
+<?php
+//LEMBRAR de nomear diferentemente variáveis e parâmetros
+function calcularMedia( $n1, $n2 ){
+    // VERSÃO 1 (variável local)
+    // $media = ($n1 + $n2) / 2;
+    // return $media;
 
+    // VERSÃO 2 (retorno implícito)
+    return ($n1 + $n2) / 2;
+}
+
+
+$nota1 = 0;
+$nota2 = 7;
+$media = calcularMedia($nota1, $nota2);
+
+if ($media >= 7){
+    $situacao = "aprovado"; 
+} else {
+    $situacao = "reprovado";
+}
+?>
+
+<p> <?=$media?> </p>
+<p> <?=$situacao?> </p>
+
+
+  
 
 </body>
 </html>
