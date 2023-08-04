b90fab533249fdb67750463ba7ed8f6946708c49
Leandro Rocha <leandro.aerolito@gmail.com>
2023-08-04 10:35:36 -0300

    Operador ternário
    Alterações com esta função.

--- exercicios/exercicio-05.php
+++ exercicios/exercicio-05.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 5 - Funções</title>
+
+<style>
+    .aprovado { color: blue;}
+    .reprovado { color: red;}
+
+</style>    
+
 </head>
 <body>
     <h1>Exercício 05 - FUNÇÕES</h1>    
-
     <hr>
 <?php
 //LEMBRAR de nomear diferentemente variáveis e parâmetros
-function calcularMedia( $n1, $n2 ){
+function calcularMedia(float $n1, float $n2 ){ 
     // VERSÃO 1 (variável local)
     // $media = ($n1 + $n2) / 2;
     // return $media;
@@ -20,21 +26,31 @@ function calcularMedia( $n1, $n2 ){
     return ($n1 + $n2) / 2;
 }
 
+function verificarSituacao(float $valorMedia ): string {
+    /* Sintaxe alternativa usando operador ternário "?:" */
+    return $valorMedia >= 7 ?"aprovado": "reprovado";
 
-$nota1 = 0;
+    /*if ($valorMedia >= 7){
+        return "aprovado"; 
+    } else {
+        return "reprovado";
+    }*/
+
+}
+
+$nota1 = 10;
 $nota2 = 7;
 $media = calcularMedia($nota1, $nota2);
+$situacao = verificarSituacao($media);
 
-if ($media >= 7){
-    $situacao = "aprovado"; 
-} else {
-    $situacao = "reprovado";
-}
 ?>
 
 <p> <?=$media?> </p>
-<p> <?=$situacao?> </p>
+<p class="<?=$situacao?>"> <?=$situacao?> </p>
 
+<!-- <p class="aprovado"> aprovado </p>
+<p class="reprovado"> reprovado </p>
+ -->
 
   
 
