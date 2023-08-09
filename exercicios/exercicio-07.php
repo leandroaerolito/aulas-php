efb752f3eb8363c212d2fd978d89b8e3e4ca4c8a
Leandro Rocha <leandro.aerolito@gmail.com>
2023-08-09 11:54:32 -0300

    exercicio 07 em construção

---
+++ exercicios/exercicio-07.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7</title>
+</head>
+<body>
+    <h1>Exercício 07 | Desafio 09 de agosto | Mr. Tee</h1>
+    <hr>
+
+    <form action="">
+        <p>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome">
+        </p>
+    
+        <select name="fabricanete" id="fabricante">
+            <option value="">Philco</option>
+            <option value="">Samsung</option>
+            <option value="">LG</option>
+            <option value="">Semp Toshiba</option>
+        </select>
+
+        
+        <div>
+        <p>
+            <label for="">Preço</label>
+            <input type="text">
+        </p>
+
+
+        <p>
+            <label for="disponibilidade">disponibilidade</label>
+        </p>
+        <p>
+            <label for="disponibilidade1">sim</label>
+            <input type="radio" name="disponibilidade" id="disponibilidade1"> 
+        </p>
+        
+        <p>
+            <label for="disponibilidade2">não</label>
+            <input type="radio" name="disponibilidade" id="disponibilidade2"> 
+        </p>
+        </div>
+
+        <p>
+            <label for="text">Descrição</label>
+            <input type="text" name="descricao" id="descricao">
+        </p>
+        
+        <!-- Checar o enunciado para finalizar a atividade, fazendo muitas pesquisas e revisões -->
+
+
+    </form>
+
+
+</body>
+</html>
